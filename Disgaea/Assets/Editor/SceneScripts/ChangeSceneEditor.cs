38d56b8f949eb82fd71f6dcfc9c460eec0a60fac
Lordmatics <niallmaple@yahoo.co.uk>
2017-05-25 23:26:45 +0100

    Menu Item - Scene Changer Tool

---
+++ Disgaea/Assets/Editor/SceneScripts/ChangeSceneEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class ChangeSceneEditor : Editor
+{
+
+    [MenuItem("OpenScene/DevScenes/NiallScene")]
+    public static void OpenNiallScene()
+    {
+        OpenScene("NiallScene");
+    }
+
+    [MenuItem("OpenScene/DevScenes/DaneScene")]
+    public static void OpenDaneScene()
+    {
+        OpenScene("DaneScene");
+    }
+
+    static void OpenScene(string name)
+    {
+        if(EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/" + name + ".unity");
+        }
+    }
+}
