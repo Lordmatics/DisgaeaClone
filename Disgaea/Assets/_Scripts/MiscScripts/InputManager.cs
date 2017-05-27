56e0e8a27a8ecbabab5b0fa4431d4b64c3645fa0
NinjaThaum <ninjathaum@gmail.com>
2017-05-27 20:23:50 +0100

    Structured Input manager

--- Disgaea/Assets/_Scripts/MiscScripts/InputManager.cs
+++ Disgaea/Assets/_Scripts/MiscScripts/InputManager.cs
@@ -6,7 +6,8 @@ using System;
 [AddComponentMenu("Scripts/MiscScripts/InputManager")]
 public class InputManager : MonoBehaviour
 {
-
+    #region Keyboard Input
+    #region Pressed
     public static event Action qPressed;
     public static event Action ePressed;
 
@@ -18,8 +19,69 @@ public class InputManager : MonoBehaviour
     public static event Action fPressed;
 
     public static event Action spacePressed;
+    #endregion
+    #region Held
+    public static event Action qHeld;
+    public static event Action eHeld;
+
+    public static event Action wHeld;
+    public static event Action aHeld;
+    public static event Action sHeld;
+    public static event Action dHeld;
+
+    public static event Action fHeld;
+
+    public static event Action spaceHeld;
+    #endregion
+    #region Released
+    public static event Action qReleased;
+    public static event Action eReleased;
+
+    public static event Action wReleased;
+    public static event Action aReleased;
+    public static event Action sReleased;
+    public static event Action dReleased;
+
+    public static event Action fReleased;
+
+    public static event Action spaceReleased;
+    #endregion
+    #endregion
+
+    #region Mouse Input
+    #region Pressed
+    #endregion
+    #region Held
+    #endregion
+    #region Released
+    #endregion
+    #endregion
+
+    #region Joypad Input
+    #region Pressed
+    #endregion
+    #region Held
+    #endregion
+    #region Released
+    #endregion
+    #endregion
 
     void Update()
+    {
+        KeyBoardInput();
+        MouseInput();
+        JoypadInput();
+    }
+
+    #region UPDATE KEYBOARD INPUT
+    void KeyBoardInput()
+    {
+        KeyBoardPressed();
+        KeyBoardHeld();
+        KeyBoardReleased();
+    }
+
+    void KeyBoardPressed()
     {
         if (Input.GetKeyDown(KeyCode.Q))
             if (qPressed != null)
@@ -42,9 +104,79 @@ public class InputManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F))
             if (fPressed != null)
                 fPressed();
-
         if (Input.GetKeyDown(KeyCode.Space))
             if (spacePressed != null)
                 spacePressed();
     }
+
+    void KeyBoardHeld()
+    {
+        if (Input.GetKey(KeyCode.Q))
+            if (qHeld != null)
+                qHeld();
+        if (Input.GetKey(KeyCode.E))
+            if (eHeld != null)
+                eHeld();
+        if (Input.GetKey(KeyCode.W))
+            if (wHeld != null)
+                wHeld();
+        if (Input.GetKey(KeyCode.A))
+            if (aHeld != null)
+                aHeld();
+        if (Input.GetKey(KeyCode.S))
+            if (sHeld != null)
+                sHeld();
+        if (Input.GetKey(KeyCode.D))
+            if (dHeld != null)
+                dHeld();
+        if (Input.GetKey(KeyCode.F))
+            if (fHeld != null)
+                fHeld();
+        if (Input.GetKey(KeyCode.Space))
+            if (spaceHeld != null)
+                spaceHeld();
+    }
+
+    void KeyBoardReleased()
+    {
+        if (Input.GetKey(KeyCode.Q))
+            if (qReleased != null)
+                qReleased();
+        if (Input.GetKey(KeyCode.E))
+            if (eReleased != null)
+                eReleased();
+        if (Input.GetKey(KeyCode.W))
+            if (wReleased != null)
+                wReleased();
+        if (Input.GetKey(KeyCode.A))
+            if (aReleased != null)
+                aReleased();
+        if (Input.GetKey(KeyCode.S))
+            if (sReleased != null)
+                sReleased();
+        if (Input.GetKey(KeyCode.D))
+            if (dReleased != null)
+                dReleased();
+        if (Input.GetKey(KeyCode.F))
+            if (fReleased != null)
+                fReleased();
+        if (Input.GetKey(KeyCode.Space))
+            if (spaceReleased != null)
+                spaceReleased();
+    }
+    #endregion
+
+    #region UPDATE MOUSE INPUT
+    void MouseInput()
+    {
+
+    }
+    #endregion
+
+    #region UPDATE GAMEPAD INPUT
+    void JoypadInput()
+    {
+
+    }
+    #endregion
 }
