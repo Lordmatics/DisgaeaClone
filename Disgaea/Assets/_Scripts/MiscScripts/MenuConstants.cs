086a233dae8ab0e16f6c4092dee8385d2a3b782e
NinjaThaum <ninjathaum@gmail.com>
2017-05-29 18:04:50 +0100

    others needed

---
+++ Disgaea/Assets/_Scripts/MiscScripts/MenuConstants.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuConstants : ScriptableObject
+{
+    public float GapBetweenNodes;
+    public float nodeHeight;
+}
